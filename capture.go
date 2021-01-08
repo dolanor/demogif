5a684e5c86a4996bbebb41f9bf4490a9ad118622
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2021-01-08 23:40:57 +0100

    distribute in many goroutines

--- capture.go
+++ capture.go
@@ -26,18 +26,38 @@ func ScreenCapture(ctx context.Context, w io.Writer, fps int) {
 	Capture(ctx, w, rect, fps)
 }
 
+type sshot struct {
+	img *image.RGBA
+	ts  time.Time
+}
+type tsimg struct {
+	img *image.Paletted
+	ts  time.Time
+}
+
 // Capture captures a gif animation until the context is done.
 // The video is taken from the rect coordinates with the defined fps.
 func Capture(ctx context.Context, w io.Writer, rect image.Rectangle, fps int) {
 	freq := float64(time.Second) / float64(fps)
 	// pre-allocate 1 minute worth of animation images
 	anim := make([]image.Image, 0, fps*60)
+
+	screens := make(chan sshot, 100)
+
+	paletteds := make(chan tsimg, 100)
 	frameTicker := time.NewTicker(time.Duration(freq))
 
+	go quantize(screens, paletteds)
+	out := &gif.GIF{}
+	done := make(chan struct{})
+	go gifer(paletteds, out, done)
+
+	i := 0
 loop:
 	for range frameTicker.C {
 		select {
 		case <-ctx.Done():
+			close(screens)
 			break loop
 		default:
 		}
@@ -45,10 +65,16 @@ loop:
 		if err != nil {
 			log.Fatal(err)
 		}
-		anim = append(anim, sc)
+		println("screen", i)
+
+		//anim = append(anim, sc)
+		screens <- sshot{
+			img: sc,
+			ts:  time.Now(),
+		}
+		i++
 	}
 
-	out := &gif.GIF{}
 	var wg sync.WaitGroup
 	concurrency := runtime.NumCPU() * 2
 	pool := make(chan struct{}, concurrency)
@@ -73,8 +99,64 @@ loop:
 	}
 	wg.Wait()
 
+	<-done
 	err := gif.EncodeAll(w, out)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func quantize(screens <-chan sshot, paletteds chan<- tsimg) {
+	i := 0
+	concurrency := runtime.NumCPU() * 2
+	pool := make(chan struct{}, concurrency)
+	for i := 0; i < concurrency; i++ {
+		pool <- struct{}{}
+	}
+
+	var wg sync.WaitGroup
+	quantizer := gogif.MedianCutQuantizer{NumColor: 64}
+	for sc := range screens {
+		<-pool
+		sc := sc
+		wg.Add(1)
+		go func() {
+			pImg := image.NewPaletted(sc.img.Bounds(), nil)
+			quantizer.Quantize(pImg, sc.img.Bounds(), sc.img, image.ZP)
+			paletteds <- tsimg{
+				img: pImg,
+				ts:  sc.ts,
+			}
+			log.Println("palette", i)
+			i++
+			pool <- struct{}{}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	println("closing paletteds")
+	close(paletteds)
+}
+
+func gifer(imgs <-chan tsimg, out *gif.GIF, done chan<- struct{}) {
+	concurrency := runtime.NumCPU() * 2
+	pool := make(chan struct{}, concurrency)
+	for i := 0; i < concurrency; i++ {
+		pool <- struct{}{}
+	}
+
+	i := 0
+	for img := range imgs {
+		log.Println("gif", i)
+		img := img
+		<-pool
+		go func() {
+			out.Image = append(out.Image, img.img)
+			out.Delay = append(out.Delay, 0)
+			i++
+			pool <- struct{}{}
+		}()
+	}
+	done <- struct{}{}
+	close(done)
 }
